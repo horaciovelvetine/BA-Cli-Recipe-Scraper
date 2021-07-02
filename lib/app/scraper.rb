e31b79890d5a2efcd387618547773c41d8288a0e
Horacio Velvatine <james@itme-music.com>
2021-07-02 17:49:09 -0600

    Reformat to scrape all details in 1 action

--- lib/app/scraper.rb
+++ lib/app/scraper.rb
@@ -18,38 +18,65 @@ class Scraper
         #puts listicle_title
 
         #---Sets scraper to creating individual recipes---#
-        individual_element_scrape(listicle_html_parsed, parent_listicle)
+        first_element_scrape(listicle_html_parsed, parent_listicle)
 
         
 
     end
 
-    def self.individual_element_scrape(listicle_html_parsed, parent_listicle)
+    def self.first_element_scrape(listicle_html_parsed, parent_listicle)
         #--Scrapes the first layer of information from listicle--#
-        individual_recipe_elements = listicle_html_parsed.css('.gallery-slide__content')
-        individual_recipe_elements.each do |recipe_element|
-            rec_title = recipe_element.css('.gallery-slide-caption__hed-text').text
-            rec_blurb = recipe_element.css('.gallery-slide-caption__dek').text
-            rec_url = recipe_element.css('.external-link').attr("href")
+        first_recipe_elements = listicle_html_parsed.css('.gallery-slide__content')
+        first_recipe_elements.each do |recipe|
+            rec_title = recipe.css('.gallery-slide-caption__hed-text').text
+            rec_blurb = recipe.css('.gallery-slide-caption__dek').text
+            rec_url = recipe.css('.external-link').attr("href")
 
-            Recipe.create(rec_title, rec_blurb, rec_url, parent_listicle)
+            more_info_details(rec_title, rec_blurb, rec_url)
+                        
+
+            
+        end
+        # puts "got here"
+    # binding.pry
+    # Cli.loading_finish
+    # Cli.explore_listicle_menu(parent_listicle)    
+    end
+
+    def self.more_info_details(rec_title, rec_blurb, rec_url)
+            url = rec_url
+            html = open(url)
+            more_info_parsed = Nokogiri::HTML(html)
+            rec_rating = more_info_parsed.css('.isYfga').text
+            rec_author = more_info_parsed.css('.byline__name').css('a').first.text
+            rec_servings = more_info_parsed.css('.flNKQv').text
+
+            
+
+
             #---DO NOT REMOVE---#
-            #Prints out each bit of information
+            #Prints out each bit of informatio
             # puts rec_title
             # puts rec_blurb
             # puts "#{rec_url}"
-            
+            # puts rec_rating
+            # puts rec_author
+            # puts rec_servings
 
-        end
-        # binding.pry
-        Cli.loading_finish
-        Cli.explore_listicle_menu(parent_listicle)
+            # binding.pry
+            
+            #not really yet, because were gonna have to figure out ingredients / steps
+            # Recipe.create(rec_title, rec_blurb, rec_url, parent_listicle)
+            
+            
     end
 
-    def self.get_more_info(more_info_link, parent_recipe)
-        url = more_info_link
-        html = open(url)
-        recipe_more_info_parsed = Nokogiri::HTML(html)
+
+
+    # def self.get_more_info(more_info_link, parent_recipe)
+    #     url = more_info_link
+    #     html = open(url)
+    #     recipe_more_info_parsed = Nokogiri::HTML(html)
 
         #Attributes to set:
         # rating, total_prep_time, num_servings, author. ingredients[], steps[]
@@ -57,7 +84,11 @@ class Scraper
 
 
         #classes (as narrow scoped as possible, zoom out + add'l tags??)
+        
         #@rating: sc-pNWdM sc-jrsJWt sc-bgPuHN lfZoIg kVXRTs isYfga .text
+        
+
+
         #@total_time_to_prepare: sc-pNWdM sc-jrsJWt sc-QBqQR lfZoIg dvVxgI .text
         #@num_of_servings: sc-pNWdM sc-jrsJWt sc-czWueg lfZoIg dvVxgI flNKQv .text
         #@author: sc-pNWdM sc-jrsJWt sc-kEqXSa sc-fxFQKN lfZoIg gbRvFD gXETNs crwdyJ byline__name-link button .text
@@ -75,8 +106,8 @@ class Scraper
 
 
 
-    end
-
+    # end
+    
 
 end
 
